927792cb699c8f3ffc025e43ae3194e7712ef7e8
jadhavsujit4 <jadhav.sujit4@gmail.com>
2020-05-28 00:12:55 +0530

    Updates

---
+++ precache-manifest.cd73637621044f3a8c3faa3809477f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42097ead38a3b8ad785545c43feeea2d",
+    "url": "/react-js-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f3d086dd8b4a78d16e18",
+    "url": "/react-js-monsters-rolodex/static/css/main.4d43a1ca.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/react-js-monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-js-monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d086dd8b4a78d16e18",
+    "url": "/react-js-monsters-rolodex/static/js/main.977d5a8e.chunk.js"
+  },
+  {
+    "revision": "d23d84df9119ed6e8e42",
+    "url": "/react-js-monsters-rolodex/static/js/runtime-main.8158d484.js"
+  }
+]);
